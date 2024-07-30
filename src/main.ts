021a9650a70e050f7f269834dc9ba256ae26a021
Jetrca92 <blaz.guzej@gmail.com>
2024-07-30 22:02:26 +0200

    Setup project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './modules/app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+
+  const PORT = process.env.PORT || 8080
+  await app.listen(PORT)
+}
+bootstrap()
